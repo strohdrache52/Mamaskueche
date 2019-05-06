32c115acafc078801f6caf39b05529c7e91991d8
philipp <philipp@141.87.56.122>
2019-05-06 16:10:15 +0200

    habs anna gezeigt

--- Mamaskueche/src/Test.java
+++ Mamaskueche/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallöle");
+		System.out.println("Hallo zeige anna das pushen");
 	}
 
 }
